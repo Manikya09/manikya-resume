7d9e2f568a812e7328b9824216cf03756d6d9076
Manikya <manikya.k09@gmail.com>
2020-10-15 23:23:58 +0530

    Updates

---
+++ precache-manifest.3655d504b153cacb0a409f282535fae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1972d27bf1889372343e34daa64f8805",
+    "url": "/manikya-resume/index.html"
+  },
+  {
+    "revision": "4e785c23489a2d5e5603",
+    "url": "/manikya-resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f04df85ccdf166a202a4",
+    "url": "/manikya-resume/static/js/2.0b543eb1.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/manikya-resume/static/js/2.0b543eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e785c23489a2d5e5603",
+    "url": "/manikya-resume/static/js/main.da930ea1.chunk.js"
+  },
+  {
+    "revision": "c2794fc63c054664751d",
+    "url": "/manikya-resume/static/js/runtime-main.8d0feea8.js"
+  }
+]);
